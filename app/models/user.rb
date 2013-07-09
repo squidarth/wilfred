2da5de3e2ff15cd07545d50a52d6e0f68e8ed433
squidarth <sshanker220@gmail.com>
2013-07-09 00:28:19 -0700

    make password optional

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   # attr_accessible :title, :body
+
+  def password_required?
+    false
+  end
 end
