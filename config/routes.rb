a0e5e577c9f8f64b395206ff80e7f6c592aa9cbc
squidarth <sshanker220@gmail.com>
2013-07-16 15:31:07 -0700

    fixed up broken route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Wilfred::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  post "/webhook/github", to: "webhooks#github"
+  post "/webhook/create", to: "webhooks#github"
   post "/webhook/circleci", to: "webhooks#circle_ci" 
   resources :commits
 
